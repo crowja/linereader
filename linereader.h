5519345c689382d02668fc76807178e41a0a1406
John Crow <johna.crow@corteva.com>
2020-01-27 12:36:29 -0600

    Bump version: 1.1.0-dev0 → 1.1.1-dev0

--- linereader.h
+++ linereader.h
@@ -1,6 +1,6 @@
 /**
  *  @file linereader.h
- *  @version 1.1.0-dev0
+ *  @version 1.1.1-dev0
  *  @see https://github.com/crowja/linereader
  *  @date Sun Jan 26 19:28:47 CST 2020
  *  @copyright 2020 John A. Crow <crowja@gmail.com>
